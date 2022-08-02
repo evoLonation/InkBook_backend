7c03ad13d5e54793dc48c3d8f68d0d30b940363b
MaxLong <longyizhou2001@163.com>
2022-08-02 18:14:56 +0800

    lyz: 完成project接口

--- api/project.go
+++ api/project.go
@@ -10,7 +10,7 @@ import (
 type ProjectCreateRequest struct {
 	Name   string `json:"name"`
 	TeamID int    `json:"teamId"`
-	UserId int    `json:"userId"`
+	UserId string `json:"userId"`
 	Detail string `json:"detail"`
 	ImgURL string `json:"imgUrl"`
 }
@@ -49,16 +49,61 @@ func ProjectCreate(ctx *gin.Context) {
 		Intro:      request.Detail,
 		ImgURL:     request.ImgURL,
 	}
+	entity.Db.Create(&project)
+	entity.Db.Where("name = ? AND team_id = ?", request.Name, request.TeamID).First(&project)
+	ctx.JSON(http.StatusOK, gin.H{
+		"msg":       "项目创建成功",
+		"projectId": project.ProjectID,
+	})
 }
 
 func ProjectDelete(ctx *gin.Context) {
-	// TODO
+	var request ProjectDeleteRequest
+	if err := ctx.ShouldBindJSON(&request); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var project entity.Project
+	entity.Db.Find(&project, "project_id = ?", request.ProjectID)
+	if project == (entity.Project{}) {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": "项目不存在",
+		})
+		return
+	}
+
+	project.IsDeleted = true
+	project.DeleteTime = time.Now()
+	entity.Db.Model(&project).Where("project_id = ?", request.ProjectID).Updates(&project)
+	ctx.JSON(http.StatusOK, gin.H{
+		"msg": "项目删除成功",
+	})
 }
 
 func ProjectRename(ctx *gin.Context) {
-	// TODO
+	var request ProjectRenameRequest
+	if err := ctx.ShouldBindJSON(&request); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var project entity.Project
+	entity.Db.Find(&project, "project_id = ?", request.ProjectID)
+	if project == (entity.Project{}) {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": "项目不存在",
+		})
+		return
+	}
+
+	project.Name = request.NewName
+	entity.Db.Model(&project).Where("project_id = ?", request.ProjectID).Updates(&project)
+	ctx.JSON(http.StatusOK, gin.H{
+		"msg": "项目重命名成功",
+	})
 }
 
 func ProjectList(ctx *gin.Context) {
-	// TODO
+
 }
--- entity/document.go
+++ entity/document.go
@@ -6,12 +6,12 @@ type Document struct {
 	DocID      int       `gorm:"column:doc_id" json:"docId"`
 	Name       string    `gorm:"column:name" json:"name"`
 	ProjectID  int       `gorm:"column:project_id" json:"projectId"`
-	CreatorID  int       `gorm:"column:creator_id" json:"creatorId"`
+	CreatorID  string    `gorm:"column:creator_id" json:"creatorId"`
 	CreateTime time.Time `gorm:"column:create_time" json:"createTime"`
-	ModifierID int       `gorm:"column:modifier_id" json:"modifierId"`
+	ModifierID string    `gorm:"column:modifier_id" json:"modifierId"`
 	ModifyTime time.Time `gorm:"column:modify_time" json:"modifyTime"`
 	IsEditing  bool      `gorm:"column:is_editing" json:"isEditing"`
 	IsDeleted  bool      `gorm:"column:is_deleted" json:"isDeleted"`
-	DeleterID  int       `gorm:"column:deleter_id" json:"deleterId"`
+	DeleterID  string    `gorm:"column:deleter_id" json:"deleterId"`
 	DeleteTime time.Time `gorm:"column:delete_time" json:"deleteTime"`
 }

f33ce8935c5e0947da7c38f0c45e9c1d4e3aa8d6
MaxLong <longyizhou2001@163.com>
2022-08-03 12:37:04 +0800

    lyz: 修改api

--- api/server.go
+++ api/server.go
@@ -54,7 +54,7 @@ func Start(address string) {
 		projectGroup.POST("/modify/intro", ProjectModifyIntro)
 		projectGroup.POST("/modify/img", ProjectModifyImg)
 		projectGroup.GET("/list", ProjectList)
-		projectGroup.GET("/list/recycle", ProjectListRecycle)
+		projectGroup.GET("/recycle", ProjectListRecycle)
 	}
 
 	//document
@@ -64,7 +64,7 @@ func Start(address string) {
 		documentGroup.POST("/delete", DocumentDelete)
 		documentGroup.POST("/complete-delete", DocumentCompleteDelete)
 		documentGroup.GET("/list", DocumentList)
-		documentGroup.GET("/list/recycle", DocumentListRecycle)
+		documentGroup.GET("/recycle", DocumentListRecycle)
 	}
 
 	err := router.Run(address)
